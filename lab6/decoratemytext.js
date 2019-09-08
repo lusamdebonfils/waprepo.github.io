bd1d7d6bbdcbeddefbc3982c4e8d7a50f487ecee
lusamdebonfils <samuelluswata@gmail.com>
2019-09-08 09:41:53 -0500

    Commiting assignment 6 initial repo

---
+++ lab6/decoratemytext.js
@@ -0,0 +1,10 @@
+
+window.onload = function(){
+  document.getElementById('button1').onclick = popFunc;
+};
+
+function popFunc(){
+  //alert("Hello, world!");
+  document.getElementById('txtarea').style.fontSize = '24pt';
+  
+}
