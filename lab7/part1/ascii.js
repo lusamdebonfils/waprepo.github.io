85e0f121c223ff5678eff52df64ea587db528cf6
lusamdebonfils <samuelluswata@gmail.com>
2019-09-13 10:45:33 -0500

    All my files got lost in the mix and i had to redo some of the work. Pushing current stuff

--- lab7/part1/ascii.js
+++ lab7/part1/ascii.js
@@ -3,7 +3,7 @@ window.onload = function(){
   document.getElementById('start').onclick=startAnimation;
   document.getElementById('stop').onclick=stopAnimation;
   document.getElementById('fontsize').onchange=increasingFont;
-  document.getElementById('turbo').onchange=increasingFont;
+  document.getElementById('turbo').onchange=tubroCharge;
 
 }
 
@@ -11,14 +11,14 @@ function startAnimation(){
   let animationChoice = document.getElementById('animation').value;
   let currentAnimation = document.getElementById('text-area');
   let animationTypeFrames = ANIMATIONS[animationChoice].split('=====\n');
-  setInterval(changingSpeedPerFrame(animationTypeFrames,250),10);
+  setInterval(changingSpeedPerFrame(animationTypeFrames,250),((animationTypeFrames.length * 250)+10));
 
 }
 
 function changingSpeedPerFrame(animationType,speed){
   return function(){
     for(let frame of animationType){
-      timer = setInterval(function(){
+      timer = setTimeout(function(){
         document.getElementById('text-area').value = frame;
       },speed);
     }
@@ -34,5 +34,5 @@ function increasingFont(textSize){
 }
 
 function tubroCharge(){
-
+  setInterval(changingSpeedPerFrame(animationTypeFrames,50),((animationTypeFrames.length * 50)+0));
 }
